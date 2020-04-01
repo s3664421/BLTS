53246ce90becf5de145976871186ae88434e6d01
Liam O'Shea <s3599835@student.rmit.edu.au>
2020-04-01 19:58:46 +1100

    Security configuration for data reading tested and verified

--- main/src/main/java/com/smartplant/app/config/SecurityConfiguration.java
+++ main/src/main/java/com/smartplant/app/config/SecurityConfiguration.java
@@ -82,7 +82,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
-            .antMatchers("api/data-readings").permitAll()
+            .antMatchers("/api/data-readings").permitAll()
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
